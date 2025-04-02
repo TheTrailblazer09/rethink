9ef4b91028033ecd50a8083b10e8629f3a8daf17
Vani Gupta <59418361+TheTrailblazer09@users.noreply.github.com>
2025-04-01 22:22:57 -0700

    added success message

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,21 @@ export default function Home() {
     const [portraitZip, setPortraitZip] = useState(null);
     const [candidZip, setCandidZip] = useState(null);
     const [error, setError] = useState("");
+    const [candidLabel,setcandidLabel]=useState("Upload Portraits (.zip)");
+    const [portraitLabel, setPortraitLabel]=useState(" Upload Candids (.zip)")
+    
 
     const handleFileChange = (e, isPortrait) => {
+       console.log("here")
         const file = e.target.files[0];
         if (file && file.name.endsWith(".zip")) {
             setError("");
             if (isPortrait) {
                 setPortraitZip(file);
+                setPortraitLabel("Potraits Uploaded Sucessfully")
             } else {
                 setCandidZip(file);
+                setcandidLabel("Candids Uploaded Sucessfully")
             }
         } else {
             setError("Only .zip files are allowed!");
@@ -52,13 +58,13 @@ export default function Home() {
                 {error && <p className="error-message">{error}</p>}
                 <div className="upload-box">
                     <label className="upload-label">
-                        Upload Portraits (.zip)
+                        {portraitLabel}
                         <input type="file" accept=".zip" className="hidden-input"
                             onChange={e => handleFileChange(e, true)}
                         />
                     </label>
                     <label className="upload-label">
-                        Upload Candids (.zip)
+                        {candidLabel}
                         <input type="file" accept=".zip" className="hidden-input"
                             onChange={e => handleFileChange(e, false)}
                         />
